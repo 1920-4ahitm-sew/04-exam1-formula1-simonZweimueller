c415de5adaf4d907c6fb52b0ee4f495a9a8bc73c
simonZweimueller <simon.zweimueller1gmail.com>
2019-11-15 15:39:01 +0100

    added function getPointsSumOfDriver

--- src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
+++ src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
@@ -1,28 +1,40 @@
 package at.htl.formula1.boundary;
 
 import at.htl.formula1.entity.Driver;
+import at.htl.formula1.entity.Result;
 
-import javax.json.Json;
-import javax.json.JsonObject;
+import javax.json.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-
+@Path("results")
 public class ResultsEndpoint {
 
+
+    @PersistenceContext
+    EntityManager em;
 
     /**
      * @param name als QueryParam einzulesen
      * @return JsonObject
      */
-    public JsonObject getPointsSumOfDriver(
-            String name
-    ) {
-        return null;
+    @GET
+    public JsonObject getPointsSumOfDriver(@QueryParam("name") String name) {
+        Long points = em.createNamedQuery("Result.getPointsSumOfDriver", Long.class).setParameter("NAME", name).getSingleResult();
+
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("driver", name)
+                .add("points", points)
+                .build();
+
+        return jsonObject;
     }
 
     /**
